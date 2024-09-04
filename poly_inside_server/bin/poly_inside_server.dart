0cc423689e58fd827cd7083009cc76f4c14b591c
goxxxa <g.o.molodtsov@gmail.com>
2024-09-04 17:36:32 +0300

    first commit

--- poly_inside_server/bin/poly_inside_server.dart
+++
@@ -1,5 +0,0 @@
-import 'package:poly_inside_server/poly_inside_server.dart' as poly_inside_server;
-
-void main(List<String> arguments) {
-  print('Hello world: ${poly_inside_server.calculate()}!');
-}
--- poly_inside_server/test/poly_inside_server_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:poly_inside_server/poly_inside_server.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
