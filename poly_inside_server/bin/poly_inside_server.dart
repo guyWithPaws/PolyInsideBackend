5c7e4a496c3cc03120edd505682fda9eabe01f68
SkyLorxd <paveldolgy2@gmail.com>
2024-09-04 17:21:47 +0300

    created base dart proj

---
+++ poly_inside_server/bin/poly_inside_server.dart
@@ -0,0 +1,5 @@
+import 'package:poly_inside_server/poly_inside_server.dart' as poly_inside_server;
+
+void main(List<String> arguments) {
+  print('Hello world: ${poly_inside_server.calculate()}!');
+}
---
+++ poly_inside_server/test/poly_inside_server_test.dart
@@ -0,0 +1,8 @@
+import 'package:poly_inside_server/poly_inside_server.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
