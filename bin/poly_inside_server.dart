580a263309a9e7b855dd3605f686c9a6f6f78636
Arseniy <19200508did@gmail.com>
2024-09-05 23:00:56 +0300

    Created base service

--- bin/poly_inside_server.dart
+++ bin/poly_inside_server.dart
@@ -1,5 +1,3 @@
-import 'package:poly_inside_server/poly_inside_server.dart' as poly_inside_server;
-
-void main(List<String> arguments) {
-  print('Hello world: ${poly_inside_server.calculate()}!');
+void main(List<String> args) {
+  
 }
