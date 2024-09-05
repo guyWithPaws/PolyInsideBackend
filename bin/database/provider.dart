45292a36b60d5f0ebd4e58ca964d6e8753d4866a
Arseniy <19200508did@gmail.com>
2024-09-05 23:18:10 +0300

    Created Database Provider

--- bin/database/provider.dart
+++ bin/database/provider.dart
@@ -1,11 +1,9 @@
 import 'database.dart';
 
 abstract interface class DatabaseProvider{
-
-  Stream<Professor> getAllProfessors();
-  Stream<Review> getAllReviewsByProfessor();
-  Stream<Professor> findProfessorByName(String name);
-  Stream<Review> getAllReviewByUser(int userId);
+  Stream<List<Professor>> getAllProfessors();
+  Stream<List<Review>> getAllReviewsByProfessor(int professorId);
+  Stream<List<Professor>> findProfessorByName(String name);
+  Stream<List<Review>> getAllReviewByUser(int userId);
   Future<void> sendReview(Review review);
-  
 }
